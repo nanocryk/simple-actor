7cebeda5dacb507f836716fe978c18cab47798b2
nanocryk <6422796+nanocryk@users.noreply.github.com>
2022-07-14 12:43:40 +0200

    invoke_async holds state across await points

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # simple-actor
 
 Provides an `Actor` type that wraps a state and allows mutating it
-in turns using `invoke`.
+in turns using `invoke` and `invoke_async`.
 
-## Example
+# Example
 
-It is recommended to create a wrapper type around the `Actor`,
-and implement async functions that use `invoke` to interact with
-the inner private state.
+It is recommended to create a wrapper type around the `Actor`, and
+implement async functions that use `invoke`/`invoke_async` to interact
+with the inner private state.
 
 ```rust
+use std::time::Duration;
 use simple_actor::Actor;
+use futures::FutureExt;
 
 #[derive(Clone)]
 pub struct Adder(Actor<u32>);
@@ -23,7 +25,27 @@ impl Adder {
     }
 
     pub async fn add(&self, x: u32) {
-        let _ = self.0.invoke(move |state| *state += x).await;
+        let _ = self.0.invoke(move |state| {
+            // We can update the state.
+            *state += x
+        }).await;
+    }
+
+    pub async fn add_twice_with_delay(&self, x: u32) -> Option<u32> {
+        self.0
+            .invoke_async(move |state| {
+                async move {
+                    *state += x;
+                    // We can .await while holding the state.
+                    tokio::time::sleep(Duration::from_millis(500)).await;
+
+                    *state += x;
+                    // We can return a value at the end.
+                    *state
+                }
+                .boxed()
+            })
+            .await
     }
 
     pub async fn result(&self) -> Option<u32> {
@@ -45,6 +67,9 @@ async fn main() {
     adder.add(2).await;
     assert_eq!(adder.result().await, Some(10));
 
+    assert_eq!(adder.add_twice_with_delay(3).await, Some(16));
+    assert_eq!(adder.result().await, Some(16));
+
     adder.shutdown();
     assert_eq!(adder.result().await, None);
 }
@@ -53,7 +78,12 @@ async fn main() {
 ## Inspiration
 
 This crate is inspired by [`ghost_actor`], with a simpler implementation and
-API. This crate `invoke` function returns `None` if the actor is down, which
+API.
+
+This crate [`invoke`] function returns `None` if the actor is down, which
 avoids dealing with error type conversions.
- 
+
+It also allows to hold the state in [`invoke_async`] and thus use
+async-based state.
+
 [`ghost_actor`]: https://github.com/holochain/ghost_actor
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,19 @@
 //! Provides an [`Actor`] type that wraps a state and allows mutating it
-//! in turns using [`invoke`].
+//! in turns using [`invoke`] and [`invoke_async`].
 //!
 //! [`invoke`]: Actor::invoke
+//! [`invoke_async`]: Actor::invoke_async
 //!
 //! # Example
 //!
-//! It is recommended to create a wrapper type around the [`Actor`],
-//! and implement async functions that use [`invoke`] to interact with
-//! the inner private state.
+//! It is recommended to create a wrapper type around the [`Actor`], and
+//! implement async functions that use [`invoke`]/[`invoke_async`] to interact
+//! with the inner private state.
 //!
-//! ```
+//! ```rust
+//! use std::time::Duration;
 //! use simple_actor::Actor;
+//! use futures::FutureExt;
 //!
 //! #[derive(Clone)]
 //! pub struct Adder(Actor<u32>);
@@ -23,7 +26,27 @@
 //!     }
 //!
 //!     pub async fn add(&self, x: u32) {
-//!         let _ = self.0.invoke(move |state| *state += x).await;
+//!         let _ = self.0.invoke(move |state| {
+//!             // We can update the state.
+//!             *state += x
+//!         }).await;
+//!     }
+//!
+//!     pub async fn add_twice_with_delay(&self, x: u32) -> Option<u32> {
+//!         self.0
+//!             .invoke_async(move |state| {
+//!                 async move {
+//!                     *state += x;
+//!                     // We can .await while holding the state.
+//!                     tokio::time::sleep(Duration::from_millis(500)).await;
+//! 
+//!                     *state += x;
+//!                     // We can return a value at the end.
+//!                     *state
+//!                 }
+//!                 .boxed()
+//!             })
+//!             .await
 //!     }
 //!
 //!     pub async fn result(&self) -> Option<u32> {
@@ -45,6 +68,9 @@
 //!     adder.add(2).await;
 //!     assert_eq!(adder.result().await, Some(10));
 //!
+//!     assert_eq!(adder.add_twice_with_delay(3).await, Some(16));
+//!     assert_eq!(adder.result().await, Some(16));
+//!
 //!     adder.shutdown();
 //!     assert_eq!(adder.result().await, None);
 //! }
@@ -53,8 +79,13 @@
 //! ## Inspiration
 //!
 //! This crate is inspired by [`ghost_actor`], with a simpler implementation and
-//! API. This crate `invoke` function returns `None` if the actor is down, which
+//! API.
+//!
+//! This crate [`invoke`] function returns `None` if the actor is down, which
 //! avoids dealing with error type conversions.
+//!
+//! It also allows to hold the state in [`invoke_async`] and thus use
+//! async-based state.
 //!  
 //! [`ghost_actor`]: https://github.com/holochain/ghost_actor
 
@@ -62,9 +93,10 @@ use futures::{
     channel::{mpsc, oneshot},
     Future, FutureExt, SinkExt, StreamExt,
 };
-use std::hash::Hash;
+use std::{hash::Hash, pin::Pin};
 
-type InnerInvoke<T> = Box<dyn FnOnce(&mut T) + 'static + Send>;
+type BoxFuture<'a, T> = Pin<Box<dyn Future<Output = T> + 'a + Send>>;
+type InnerInvoke<T> = Box<dyn for<'a> FnOnce(&'a mut T) -> BoxFuture<'a, ()> + Send>;
 type SendInvoke<T> = mpsc::Sender<InnerInvoke<T>>;
 
 /// Actor wrapping a state.
@@ -85,26 +117,27 @@ impl<T: 'static + Send> Actor<T> {
     /// Returned future must be spawned in an async executor.
     #[must_use]
     pub fn new_with_capacity(mut state: T, capacity: usize) -> (Self, impl Future<Output = ()>) {
-        let (invoke_tx, invoke_rx) = mpsc::channel::<InnerInvoke<T>>(capacity);
+        let (invoke_tx, mut invoke_rx) = mpsc::channel::<InnerInvoke<T>>(capacity);
 
         let driver = FutureExt::boxed(async move {
-            let mut invoke_rx = StreamExt::ready_chunks(invoke_rx, 1024);
-
-            while let Some(invokes) = invoke_rx.next().await {
-                for invoke in invokes {
-                    invoke(&mut state);
-                }
+            while let Some(invoke) = invoke_rx.next().await {
+                invoke(&mut state).await;
             }
         });
 
         (Self(invoke_tx), driver)
     }
 
-    /// Interact with the state.
+    /// Interacts with the state using a closure returning a future.
+    /// This future holds the mutable reference to the state, and prevents the
+    /// actor to process further invokes until this future ends.
+    ///
+    /// The future needs to be boxed using [`futures::FutureExt::boxed`].
+    ///
     /// Returns `None` if the actor is no longer running.
-    pub fn invoke<F, R>(&self, invoke: F) -> impl Future<Output = Option<R>>
+    pub fn invoke_async<F, R>(&self, invoke: F) -> impl Future<Output = Option<R>>
     where
-        F: FnOnce(&mut T) -> R + 'static + Send,
+        F: for<'a> FnOnce(&'a mut T) -> BoxFuture<'a, R> + Send + 'static,
         R: 'static + Send,
     {
         let mut invoke_tx = self.0.clone();
@@ -113,8 +146,11 @@ impl<T: 'static + Send> Actor<T> {
             let (response_tx, response_rx) = oneshot::channel();
 
             let real_invoke: InnerInvoke<T> = Box::new(move |state: &mut T| {
-                let res = invoke(state);
-                let _ = response_tx.send(res);
+                async move {
+                    let res = invoke(state).await;
+                    let _ = response_tx.send(res);
+                }
+                .boxed()
             });
 
             invoke_tx.send(real_invoke).await.ok()?;
@@ -123,15 +159,16 @@ impl<T: 'static + Send> Actor<T> {
         }
     }
 
-    /// Interact with the state then awaits a Future.
-    /// This Future cannot hold the reference to the state.
-    pub async fn invoke_future<Fun, Fut, R>(&self, invoke: Fun) -> Option<R>
+    /// Interact with the state using a closure.
+    ///
+    /// Returns `None` if the actor is no longer running.
+    pub async fn invoke<F, R>(&self, invoke: F) -> Option<R>
     where
-        Fun: FnOnce(&mut T) -> Fut + 'static + Send,
-        Fut: Future<Output = R> + 'static + Send,
+        F: FnOnce(&mut T) -> R + 'static + Send,
         R: 'static + Send,
     {
-        Some(self.invoke(invoke).await?.await)
+        self.invoke_async(|state| async move { invoke(state) }.boxed())
+            .await
     }
 
     /// Tells if the actor still accepts new invokes.
